ba19fba14874c1d21c437c8df72fe09f85b640dd
danielH <110135225+danielsCodeTaco@users.noreply.github.com>
2023-02-23 16:19:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-url = document.getElementById("url").value;
+url = document.getElementById("url");
 
 function sendMessage(message) {
+    console.log(message);
     const request = new XMLHttpRequest();
     request.open("POST", "https://discord.com/api/webhooks/1078419441005641729/XwlqbpqWprCaHGSCNi-T7KzpoXUTJdRngtOUB3qdiCumASO1sJd4aLqw0dwpQUtu2iYA");
 
