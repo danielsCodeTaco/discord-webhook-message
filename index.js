943143f35b037570245c5a145aa9bdffbb4621cf
danielH <110135225+danielsCodeTaco@users.noreply.github.com>
2023-02-23 16:09:33 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+function sendMessage(message) {
+    const request = new XMLHttpRequest();
+    request.open("POST", "https://discord.com/api/webhooks/1078419441005641729/XwlqbpqWprCaHGSCNi-T7KzpoXUTJdRngtOUB3qdiCumASO1sJd4aLqw0dwpQUtu2iYA");
+
+    request.setRequestHeader('Content-type', 'application/json');
+    
+    const params = {
+        username: "poopoo",
+        avatar_url: "",
+        content: message
+    }
+
+    request.send(JSON.stringify(params));
+}
